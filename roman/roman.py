aebc08f16c041ad2f1a3d9f31e929760bb79aba0
Osvaldo Santana <osantana@triveos.com>
2010-10-02 01:19:03 -0300

    refatorado 44->XLXV para remover duplicação

--- roman/roman.py
+++ roman/roman.py
@@ -1,19 +1,19 @@
 # roman.py
 
 def to_roman(decimal):
-    values = {
-        40: "XL",
-        10: "X",
-         9: "IX",
-         5: "V",
-         4: "IV",
-         1: "I",
-    }
+    values = (
+        (40, "XL"),
+        (10, "X"),
+        ( 9, "IX"),
+        ( 5, "V"),
+        ( 4, "IV"),
+        ( 1, "I"),
+    )
 
-    if decimal in values:
-        return values[decimal]
+    for d, v in values:
+        if decimal == d:
+            return v
 
-    for d in (40, 10, 9, 5, 4, 1):
         if decimal > d:
             return to_roman(d) + to_roman(decimal - d)
 
