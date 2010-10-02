4b82b998eb48c8eba73b5f574654f898f75e9970
Osvaldo Santana <osantana@triveos.com>
2010-10-02 00:37:07 -0300

    teste 1->I passando

--- roman/roman.py
+++ roman/roman.py
@@ -1,4 +1,6 @@
 # roman.py
 
 def to_roman(decimal):
+    if decimal == 1:
+        return "I"
     return ""
--- roman/test_roman.py
+++ roman/test_roman.py
@@ -5,3 +5,7 @@ from roman import to_roman
 class TestRoman(unittest.TestCase):
     def test_zero(self):
         self.assertEquals("", to_roman(0))
+
+    def test_one(self):
+        self.assertEquals("I", to_roman(1))
+
