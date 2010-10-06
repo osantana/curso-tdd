db94ec091e359efb994164f9a05245f49f70f8aa
Osvaldo Santana <osantana@triveos.com>
2010-10-06 20:11:14 -0300

    Teste de POST passando

--- code-bin/codebin/__init__.py
+++ code-bin/codebin/__init__.py
@@ -1,8 +1,35 @@
-from google.appengine.ext import webapp
+import os
 
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+from django import newforms as forms
+
+
+LANGS = (
+    ('plain', "Plain Text"),
+    ('python', "Python"),
+    ('ruby', "Ruby"),
+    ('javascript', "Javascript"),
+    ('html', "HTML"),
+)
+
+class CodeForm(forms.Form):
+    code = forms.CharField(widget=forms.Textarea())
+    lang = forms.ChoiceField(choices=LANGS)
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        template_values = {
+            'form': CodeForm(),
+        }
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
 
-
+    def post(self):
+        code = self.request.get("code", "")
+        template_values = {
+            'code': code,
+        }
+        path = os.path.join(os.path.dirname(__file__), 'code.html')
+        self.response.out.write(template.render(path, template_values))
--- code-bin/tests/test_web.py
+++ code-bin/tests/test_web.py
@@ -1,6 +1,7 @@
 import unittest
 
 from webtest import TestApp
+
 from main import application
 
 
@@ -10,5 +11,24 @@ class WebTest(unittest.TestCase):
 
     def test_index(self):
         response = self.app.get('/')
-        assert 'Hello world!' in str(response)
+        form = response.html.form
+        self.assertEquals("code", form.find(name="textarea")['name'])
+        self.assertEquals("lang", form.find(name="select")['name'])
+        self.assertEquals("submit", form.find(name="button")['type'])
 
+    def test_languages(self):
+        langs = [ "plain", "python", "ruby", "javascript", "html" ]
+        response = self.app.get("/")
+        form_langs = []
+        for opt in response.html.form.select.findAll():
+            form_langs.append(opt['value'])
+        self.assertEquals(langs, form_langs)
+
+    def test_submit_plaintext(self):
+        response = self.app.post('/', {
+            'code': "Hello world!",
+            'lang': "plain",
+        })
+
+        pre = response.html.pre
+        self.assertEquals("Hello world!", pre.string)
