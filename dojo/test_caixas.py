3af56484fbcaa12497316f7e23429c2308060171
Osvaldo Santana <osantana@triveos.com>
2010-10-08 19:17:34 -0300

    Dojo segundo teste passando

--- dojo/test_caixas.py
+++ dojo/test_caixas.py
@@ -1,7 +1,23 @@
 import unittest
 
 def somapilha(caixas):
-    return caixas[0][0]
+    if len(caixas) == 2:
+        if caixas[1][1] >= caixas[0][1]:
+            return caixas[0][0] + caixas[1][0]
+        else:
+            return caixas[0][0] + (caixas[1][0] - caixas[0][0])
+    else:
+        return caixas[0][0]
+
+def somapilha2(caixas):
+    altura = 0
+    ultima_largura = 0
+    for caixa in caixas:
+        if ultima_largura <= caixa[1]:
+            altura = altura + caixa[0]
+
+        ultima_largura = caixa[1]
+    return altura
 
 class DojoTest(unittest.TestCase):
 
@@ -9,3 +25,6 @@ class DojoTest(unittest.TestCase):
         caixas = [(10,15)]
         self.assertEquals(10, somapilha(caixas))
 
+    def test_dois_caixa(self):
+        caixas = [(10,15),(15,5)]
+        self.assertEquals(15, somapilha(caixas))
